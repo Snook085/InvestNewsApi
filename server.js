3f2c1b40b2b75719d92245966e65ff7e7413192a
Snook085 <deivid21y@gmail.com>
2024-11-03 19:40:32 -0300

    Resolvendo conflito

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ import cors from 'cors';
 const server = express();
 const noticiasBitcoins = [];
 
-// Função assíncrona para carregar os dados
 async function puxaDados() {
     try {
         const data = await fs.readFile('./Bitcoins.json', 'utf-8');
@@ -13,28 +12,28 @@ async function puxaDados() {
         jsonData.forEach(element => {
             noticiasBitcoins.push(element);
         });
-        console.log(noticiasBitcoins); // Log dos dados carregados
+        console.log(noticiasBitcoins); 
     } catch (err) {
         console.log('Erro ao ler arquivo', err.message);
     }
 }
 
-// Chama a função e aguarda o carregamento antes de iniciar o servidor
+
 puxaDados().then(() => {
     server.use(express.json());
     server.use(cors());
 
     server.get('/', (req, res) => {
-        res.json(noticiasBitcoins); // Retorna o array já preenchido
+        res.json(noticiasBitcoins);
     });
 
     server.get('/:id', (req, res) => {
         const { id } = req.params;
         const noticiaIndex = noticiasBitcoins.findIndex(noticia => noticia.id === +id);
         if (noticiaIndex !== -1) {
-            res.json(noticiasBitcoins[noticiaIndex]); // Retorna a notícia correspondente
+            res.json(noticiasBitcoins[noticiaIndex]);
         } else {
-            res.status(404).json({ message: 'Notícia não encontrada' }); // Lida com ID inválido
+            res.status(404).json({ message: 'Notícia não encontrada' });
         }
     });
 
